b9da28ff5bab0d935ad983a0d78f8af9eb3f5f00
Nikolay Tymtsiv <ntymtsiv@mirantis.com>
2015-07-28 01:49:00 +0300

    Functional tests for single tenant

--- devlab/tests/filtering_utils.py
+++ devlab/tests/filtering_utils.py
@@ -8,7 +8,8 @@ class FilteringUtils:
         # TODO:
         #  Using relative paths is a bad practice, unfortunately this is the
         #  only way at this moment.
-        #  Should be fixed by implementing proper package module for Cloud Ferry.
+        #  Should be fixed by implementing proper package module for
+        #  Cloud Ferry.
         self.main_folder = os.path.dirname(os.path.dirname(os.getcwd()))
 
     def load_file(self):
@@ -22,6 +23,8 @@ class FilteringUtils:
         loaded_data = self.load_file()
         filter_dict = loaded_data[0]
         popped_vm_list = []
+        if 'instances' not in filter_dict:
+            return [src_data_list, []]
         for vm in src_data_list[:]:
             if vm['id'] not in filter_dict['instances']['id']:
                 popped_vm_list.append(vm)
@@ -35,6 +38,8 @@ class FilteringUtils:
         popped_img_list = []
         default_img = 'Cirros 0.3.0 x86_64'
         src_data_list = [x.__dict__ for x in src_data_list]
+        if 'images' not in filter_dict:
+            return [src_data_list, []]
         for img in src_data_list[:]:
             if img['id'] not in filter_dict['images']['images_list']:
                 if img['name'] != default_img:
@@ -42,3 +47,22 @@ class FilteringUtils:
                     index = src_data_list.index(img)
                     src_data_list.pop(index)
         return [src_data_list, popped_img_list]
+
+    def get_resource_names(self, obj, cfg):
+        if obj == 'routers':
+            return [i['router']['name'] for i in cfg]
+        else:
+            return [i['name'] for i in cfg]
+
+    def get_resources_from_config(self, res):
+        if res == 'security_groups':
+            return [sg for i in config.tenants if 'security_groups' in i
+                    for sg in i['security_groups']]
+        elif res == 'servers':
+            cfg = getattr(config, 'vms')
+            [cfg.extend(i['vms']) for i in config.tenants if 'vms' in i]
+            return cfg
+        elif res == 'volumes':
+            res = 'cinder_volumes'
+
+        return getattr(config, res)
